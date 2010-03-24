566ae612dff4b1923d07c75c6c0bac9956275b42
jfbrais@gmail.com <jfbrais@gmail.com@31f2716b-a433-67fe-0bfc-f04287bf23a8>
2010-03-24 06:08:56 +0000

    Plusieurs modifications au niveau du fonctionnement.
    Il faut implanter une fonction d'authentification au moment de la connexion au serveur.

--- trunk/KeyXClient/src/core/ClientTest.java
+++ trunk/KeyXClient/src/core/ClientTest.java
@@ -1,3 +1,23 @@
+/******************************************************
+ Compagnie :	Transax Technologies
+ Projet :		KeyExchangeClient
+ Auteurs : 		Jean-François Brais-Villemur, Analyste Réseau
+ 				Marc-André Laporte, Programmeur Analyste
+ Superviseur :	Alain Boucher, CTO
+ Classe :		ClientTest.java			 
+ Création  :	2010-03-08
+ Dern. mod : 	2010-03-23
+ *******************************************************
+ Historique des modifications
+ *******************************************************
+ 2010-03-08 :	Début du projet
+ 
+ 2010-03-17 :	Ajout d'un TerminalID
+ 
+ 2010-03-23 : 	Envoi d'un message d'authentification
+ 				une fois la connexion établie.
+ *******************************************************/
+
 package core;
 
 public class ClientTest {
@@ -9,7 +29,7 @@ public class ClientTest {
 		client.connect(client.getIP(), client.getPort());
 		
 		msg.sendMessage(client.getTerminalID(), client.getOutputStream());
-		msg.sendMessage("KEYX", client.getOutputStream());
-		msg.sendMessage("QUIT", client.getOutputStream());
+//		msg.sendMessage("KEYX", client.getOutputStream());
+//		msg.sendMessage("QUIT", client.getOutputStream());
 	}
 }
